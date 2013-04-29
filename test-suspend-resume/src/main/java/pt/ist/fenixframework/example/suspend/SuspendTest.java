483a4e0c7c3dcff56a45b4b4afb00e1d39f55f38
Hugo Correia <hfcorreia91@gmail.com>
2013-04-29 11:04:55 +0100

    Update test-suspend-resume example to work with latest 2.1-SNAPSHOT

---
+++ test-suspend-resume/src/main/java/pt/ist/fenixframework/example/suspend/SuspendTest.java
@@ -0,0 +1,186 @@
+package pt.ist.fenixframework.example.suspend;
+
+import java.util.Scanner;
+
+import javax.transaction.SystemException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+import pt.ist.fenixframework.Transaction;
+import pt.ist.fenixframework.TransactionManager;
+import pt.ist.fenixframework.example.suspend.domain.App;
+import pt.ist.fenixframework.example.suspend.domain.Person;
+
+public class SuspendTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(SuspendTest.class);
+
+    public static void main(final String[] args) {
+        try {
+            logger.info("Will start the repl");
+            new SuspendTest().repl();
+        } finally {
+            FenixFramework.shutdown();
+        }
+    }
+
+    private TransactionManager tm;
+    private Transaction tx;
+    private App app;
+
+    SuspendTest() {
+        tm = FenixFramework.getTransactionManager();
+        tx = null;
+    }
+
+    @Atomic
+    private void init() {
+        logger.info("app is null");
+        App app = new App();
+        logger.info("setting app {} on root {}", app.getExternalId(), FenixFramework.getDomainRoot().getExternalId());
+        FenixFramework.getDomainRoot().setApp(app);
+        this.app = app;
+    }
+
+    private void repl() {
+        init();
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String cmd = scanner.next();
+
+            try {
+                if (cmd.equals("quit")) {
+                    rollback();
+                    return;
+                } else if (cmd.equals("list")) {
+                    listPeople();
+                } else if (cmd.equals("add")) {
+                    addPerson(scanner.nextLine().trim());
+                } else if (cmd.equals("done")) {
+                    commit();
+                } else if (cmd.equals("cancel")) {
+                    rollback();
+                } else if (cmd.equals("rename")) {
+                    rename(scanner.nextLine().trim(), scanner.nextLine().trim());
+                } else if (cmd.equals("show")) {
+                    showPerson(scanner.nextLine().trim());
+                } else {
+                    logger.info("Unknown command: {}", cmd);
+                }
+            } catch (Exception e) {
+                logger.error("Error during processing of command: {}", e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void resumeTx() {
+        if (tx == null) {
+            try {
+                tm.begin();
+            } catch (Exception e) {
+                logger.error("Begin transaction failed {}", e);
+                throw new RuntimeException("Aborting operation, because Begin failed");
+            }
+        } else {
+            boolean resumed = false;
+            try {
+                tm.resume(tx);
+                resumed = true;
+            } catch (Exception e) {
+                logger.error("Resume transaction failed {}", e);
+                throw new RuntimeException("Aborting operation, because resume failed");
+            } finally {
+                if (!resumed) {
+                    rollbackTx();
+                }
+            }
+        }
+    }
+
+    private void listPeople() {
+        resumeTx();
+
+        for (Person p : app.getPersonSet()) {
+            logger.info("{} (externalId={}, {} )", p.getName(), p.getExternalId(), p);
+        }
+
+        suspendTx();
+    }
+
+    private void addPerson(String name) {
+        resumeTx();
+        app.addPerson(new Person(name));
+        suspendTx();
+    }
+
+    private void rename(String from, String to) {
+        resumeTx();
+
+        for (Person p : app.getPersonSet()) {
+            if (p.getName().equals(from)) {
+                p.setName(to);
+            }
+        }
+
+        suspendTx();
+    }
+
+    private void showPerson(String externalId) {
+        resumeTx();
+
+        try {
+            Person p = FenixFramework.getDomainObject(externalId);
+            logger.info("{} (externalId={}, {} )", p.getName(), p.getExternalId(), p);
+        } catch (Exception e) {
+            logger.error("Can't show person with externalid ={}: {}", externalId, e);
+        }
+
+        suspendTx();
+    }
+
+    private void commit() {
+        resumeTx();
+        boolean finished = false;
+        try {
+            tm.commit();
+            finished = true;
+        } catch (Exception e) {
+            logger.error("Commit transaction failed {}", e);
+        } finally {
+            if (!finished) {
+                rollbackTx();
+            }
+            tx = null;
+        }
+    }
+
+    private void rollback() {
+        resumeTx();
+        rollbackTx();
+    }
+
+    private void rollbackTx() {
+        try {
+            tm.rollback();
+        } catch (Exception e) {
+            logger.error("Rollback transaction failed {}", e);
+            throw new RuntimeException("Aborting operation, because rollback failed");
+        } finally {
+            tx = null;
+        }
+    }
+
+    private void suspendTx() {
+        try {
+            tx = (Transaction) tm.suspend();
+        } catch (SystemException e) {
+            logger.error("Suspend transaction failed {}", e);
+            throw new RuntimeException("Aborting operation, because suspend failed");
+        }
+    }
+}
--- test-suspend-resume/src/main/java/pt/ist/fenixframework/example/suspend/domain/App.java
+++ test-suspend-resume/src/main/java/pt/ist/fenixframework/example/suspend/domain/App.java
@@ -1,7 +1,6 @@
 package pt.ist.fenixframework.example.suspend.domain;
 
 public class App extends App_Base {
-
     public App() {
         super();
     }
